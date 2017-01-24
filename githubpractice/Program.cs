af3cc83e155157214716dc2eec64f8ff56dcc440
Amie Newman <amilnewm@ut.utm.edu>
2017-01-24 16:14:34 -0600

    wow. such changes.

--- githubpractice/Program.cs
+++ githubpractice/Program.cs
@@ -10,7 +10,39 @@ namespace githubpractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int randomnumber = 0;
+            int guesses = 0;
+            //random number and number of guesses 
+            Random classs = new Random();
+            randomnumber = classs.Next(1, 101);
+
+            Console.WriteLine("Guess a number between 1~100");
+
+            string guess = Console.ReadLine();
+            int numguess = Int32.Parse(guess);
+
+            Console.WriteLine(numguess);
+
+            while (randomnumber != numguess)
+            {
+                if (randomnumber > numguess)
+                {
+                    Console.WriteLine("You guessed too low, guess again please.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed too high, guess again please.");
+                }
+                Console.WriteLine("Guess a number now: ");
+
+                guesses++;
+                guess = Console.ReadLine();
+                numguess = Int32.Parse(guess);
+            }
+
+            Console.WriteLine("YAY. FINALLY YOU DID SOMETHING RIGHT. And it only took " + guesses + " guesses." );
+              //this reaction is by no means a reflection of the programmers feelings towards its users and completely random. 
+              //it is a joke.  
             Console.ReadKey();
 
         }
